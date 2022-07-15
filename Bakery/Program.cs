8e033271f4c73fa18b570707e663ef66f79ec521
Claire Thorington <claire.thorington@gmail.com>
2022-07-15 14:48:08 -0700

    continue building program logic

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -7,16 +7,13 @@ namespace Bakery
   
   public static void Main()
   {
-    // Bread newBreadOrder = new Bread(1, 5);
-    // Pastry newPastryOrder = new Pastry(1, 2);
-
-    Console.WriteLine("Welcome to Pierre's Bakery! Today we've got a few slammin' deals for you. Bread is $5 per loaf, two loaves for $10, and we're currently running a buy 2, get 1 free special! Pastries are $2 each, 3 for $5, 4 for $7, 5 for $9, or 6 for $10. Would you like to order something? Yes/No");
+    Console.WriteLine("Welcome to Pierre's Bakery! We've got a few slammin' deals for you. Bread is $5 per loaf, two loaves for $10, and we're currently running a buy 2, get 1 free special! Pastries are $2 each, 3 for $5, 4 for $7, 5 for $9, or 6 for $10. Would you like to order something? Yes/No");
     string stringYesNoOrder = Console.ReadLine();
-    if (stringYesNoOrder == "Yes")
+    if (stringYesNoOrder.ToLower() == "yes")
     {
       Console.WriteLine("You smart cookie! Hahaha, see what we did there? Yes/No");
       string stringYesNoUnderstand = Console.ReadLine();
-      if (stringYesNoUnderstand == "Yes")
+      if (stringYesNoUnderstand.ToLower() == "yes")
       {
         Console.WriteLine("How many loaves of bread would you like to order? Please enter a number:");
         string stringNumberOfLoavesToOrder = Console.ReadLine();
@@ -24,11 +21,14 @@ namespace Bakery
         Console.WriteLine("How many pastries would you like to order? Please enter a number:");
         string stringNumberOfPastriesToOrder = Console.ReadLine();
         int intNumberOfPastriesToOrder = int.Parse(stringNumberOfPastriesToOrder);
+        orderTotal = (intNumberOfLoavesToOrder.getPrice()) + (intNumberOfPastriesToOrder.getPrice());
+        Console.WriteLine("Thanks for your order! Your total is: " + orderTotal)
       }
-      if (stringYesNoUnderstand == "No")
+      if (stringYesNoUnderstand.ToLower() == "no")
+      Console.WriteLine("Let's try this again. You're at a shop that sells baked goods. Think about what a cookie is.");
       Main();
     }
-    if (stringYesNoOrder == "No")
+    if (stringYesNoOrder.ToLower() == "no")
     {
       Console.WriteLine("Maybe you didn't hear about our slammin' deals.");
       Main();
