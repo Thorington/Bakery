1152cd6fed859c5d7ebcc15156675af6faa5d166
Claire Thorington <claire.thorington@gmail.com>
2022-07-18 21:46:05 -0700

    general WIP commit, pastry code isn't done yet

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -14,22 +14,22 @@ namespace Bakery
 
     public int GetPastryPrice(int numberOfPastries)
   {
-    if (numberOfPastries==1)
-    {
-      return 2;
-    }
-    else if (numberOfPastries%2==0)
-    {
-      return ((numberOfPastries*2)*(2/3));
-    }
-    else if (numberOfPastries%2==1)
-    {
-      return (((numberOfPastries-1)*(2/3))+2);
-    }
-    else
-    {
-      return (000000);
-    }
+    // if (numberOfPastries==1)
+    // {
+    //   return 2;
+    // }
+    // else if (numberOfPastries%2==0)
+    // {
+    //   return ((numberOfPastries*2)*(2/3));
+    // }
+    // else if (numberOfPastries%2==1)
+    // {
+    //   return (((numberOfPastries-1)*(2/3))+2);
+    // }
+    // else
+    // {
+    //   return (000000);
+    // }
   }
   }
 }
